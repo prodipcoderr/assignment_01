bfab4d3ad02fde85731642b86d7fc83baf128e09
unknown <prodipcoderr>
2024-01-08 21:23:44 +0600

    final commit

---
+++ learing.js
@@ -0,0 +1,23 @@
+var newArray = [
+  "Flora Ballard",
+  "Lula Phillips",
+  "Hannah West",
+  "Connor Wagner",
+];
+
+// newArray.push("Allie Miles");
+// console.log(newArray);
+
+// newArray.pop();
+// console.log(newArray);
+
+// newArray.shift();
+// console.log(newArray);
+
+// newArray.unshift("Prodip Karmokar");
+// console.log(newArray);
+
+console.log(newArray.indexOf("Prodip Karmokar"));
+
+newArray.splice(1);
+console.log(newArray);
